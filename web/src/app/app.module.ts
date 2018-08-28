caa7322d388602cb5389f090258f79179bca96c0
Alec Skinner <skinner452@gmail.com>
2018-08-28 12:45:35 -0500

    Issue #15

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
--- web/src/app/components/trade/trade.component.ts
+++ web/src/app/components/trade/trade.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Coin } from '../../models/coin';
+import { User } from '../../models/user';
 import { Trade } from '../../models/trade';
 
 import { ActivatedRoute } from '@angular/router';
@@ -15,7 +16,10 @@ export class TradeComponent implements OnInit {
 
   coins:Coin[];
   coin:Coin;
+  user:User;
   trades:Trade[];
+
+  buyModel = new Object();
 
   constructor(private route: ActivatedRoute, private apiService:ApiService) { }
 
@@ -33,13 +37,36 @@ export class TradeComponent implements OnInit {
 
   loadCoin(){
     if(this.coinId){
+      this.apiService.getUser(1).subscribe((user) => {
+        this.user = user;
+      });
       this.apiService.getCoin(this.coinId).subscribe((coin) => {
         this.coin = coin;
-        this.coin.createChart(this.apiService,'#3cba9f')
+        this.coin.createChart(this.apiService,'#3cba9f');
+
+        this.buyModel['value'] = coin.value;
+        this.updateBuyTotal();
       });
       this.apiService.getTrades({coinId:this.coinId}).subscribe((trades) => {
         this.trades = trades;
       });
     }
+  }
+
+  updateBuyTotal(){
+    let value = Number(this.buyModel['value']);
+    let amount = Number(this.buyModel['amount'])
+    this.buyModel['total'] = value*amount*1.01;
+  }
+
+  onBuy(){
+    this.apiService.buyCoin(this.coinId,this.buyModel['amount']).subscribe((resp) => {
+      if(resp['status'] == 'success'){
+        this.buyModel['value'] = '';
+        this.buyModel['amount'] = '';
+        this.buyModel['total'] = '';
+        this.loadCoin();
+      }
+    });
   }
 }
